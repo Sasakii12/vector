a1f503aae53774b2e8faa3c8692c100d675a15e4
Vipershroom <undertalefan812@gmail.com>
2026-06-07 01:40:27 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Vector {
+	public:
+		int size = 0;
+		int capacity = 1;
+		unique_ptr<int[]> arr = make_unique<int[]>(capacity);
+
+		void push_back(int elem) {
+			if (this->size == 0) {
+				this->size++;
+				this->arr[0] = elem;
+				return;
+			}
+
+			if (this->size > this->capacity) {
+				this->capacity *= 2;
+				int* buff_arr = new int[capacity];
+				for (int i = 0; i < size; ++i) {
+					buff_arr[i] = arr[i]; 
+				}
+				arr.reset(buff_arr);
+			}
+			this->arr[size] = elem;
+			this->size++;
+		}
+
+		int index(int x) {
+			if (0 > x || x >= this->size) {
+				return -1;
+			}
+			return arr[x];
+		}
+};
+
+int main() {
+	Vector arr;
+	arr.push_back(2);
+	arr.push_back(3);
+	arr.push_back(9);
+	for (int i = 0; i < arr.size; ++i) {
+		cout << arr.index(i) << '\n';
+	}
+}
